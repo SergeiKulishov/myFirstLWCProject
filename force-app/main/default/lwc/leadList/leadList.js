d558bf48d9e26d15dd56799628193c8f06df846e
SergeiKulishov <rewq95kso@yandex.ru>
2021-04-17 12:54:55 +0300

    done

--- force-app/main/default/lwc/leadList/leadList.js
+++ force-app/main/default/lwc/leadList/leadList.js
@@ -1,5 +1,8 @@
 import { LightningElement, track, wire } from 'lwc';
 import searchLeads from '@salesforce/apex/LeadSearchController.searchLeads';
+import {NavigationMixin} from  'lightning/navigation';
+
+const DELAY = 350;
 
 const COLS = [
     { label: 'Name', fieldName: 'Name', type:'text' },
@@ -8,29 +11,56 @@ const COLS = [
     { label: 'View', type: 'button-icon', initialWidth: 75, typeAttributes:{title:'View Details',alternativeText:'View Details',iconName:'action:info'} }
 ];
 
-export default class LeadList extends LightningElement {
-    @track leads =[];
-    @track searchterm;
+export default class LeadList extends NavigationMixin(LightningElement) {
+    @track leads = [];
+    @track searchTerm;
     @track cols = COLS;
+    @track error;
 
-    handleSearchTermChange(event){
-        this.searchterm = event.target.value;
-        const selectedEvent = new CustomEvent('newsearch', {detail: this.searchterm})
-        this.dispatchEvent(selectedEvent);
+    
+    handleSearchTermChange(event) {
+        this.searchTerm = event.target.value;
+        if (this.leads) {
+            const selectedEvent = new CustomEvent('newsearch', {detail: this.searchTerm});
+            window.clearTimeout(this.delayTimeout);
+            // eslint-disable-next-line @lwc/lwc/no-async-operation
+            this.delayTimeout = setTimeout(() => {
+                this.dispatchEvent(selectedEvent);
+            }, DELAY);
+        }
+
     }
 
     @wire(searchLeads, {
-        searchterm: '$searchTerm'
+        searchTerm: '$searchTerm'
     })
-    loadLeads({ error, data }){
-        if(data){
+    loadLeads({ error, data }) {
+        if (data) {
             this.leads = data;
+            const selectedEvent = new CustomEvent('searchcomplete', {detail: this.searchTerm});
+            this.dispatchEvent(selectedEvent);
             this.error = undefined;
-        }else if(error){
+        } else if (error) {
             this.error = error;
             this.leads = undefined;
         }
     }
+
+    handleRowAction(event) {
+        const row = event.detail.row;
+        this.record = row;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: row.Id,
+                actionName: 'view',
+            },
+        });
+    }
+
+     
+
+
 
     
 }
